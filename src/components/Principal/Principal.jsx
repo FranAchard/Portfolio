9bc1a6b651fbabb56906a6bb7684c856ab7b44a4
FranAchard <franachard8@gmail.com>
2023-04-29 05:16:59 -0300

    tengo que editar la foto

--- src/components/Principal/Principal.jsx
+++ src/components/Principal/Principal.jsx
@@ -14,6 +14,12 @@ const Principal=()=>{
                     <a href="#" className="btn">Let's Talk</a>
                 </div>
             </div>
+            <div className="home-sci">
+                <a href="https://github.com/FranAchard"><box-icon type='logo' name='github'></box-icon></a>
+                <a href="https://www.linkedin.com/in/francisco-achard-9862641b3/"><box-icon name='linkedin' type='logo' ></box-icon></a>
+                <a href="https://www.instagram.com/_fran_achard/"><box-icon name='instagram-alt' type='logo' ></box-icon></a>
+            </div>
+            <img className="foto" src="/public/assets/perfil.jpeg"></img>
         </section>
     )
 }
