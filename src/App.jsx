796661c738c7f8d4d237a4adde69a52e1f8e6f3b
FranAchard <franachard8@gmail.com>
2023-04-30 11:13:12 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 function App() {
   return (
-    <>
-      <div>
-        <Home />
-      </div>
-    </>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/about" element={ <About />} />
+      </Routes>
+    </div>
   )
 }
 
