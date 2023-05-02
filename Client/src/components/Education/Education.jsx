612bfba972fbcd3117d4ff374ba3018d3563e579
FranAchard <franachard8@gmail.com>
2023-05-02 11:45:03 -0300

    education

--- Client/src/components/Education/Education.jsx
+++ Client/src/components/Education/Education.jsx
@@ -4,8 +4,36 @@ const Education =()=>{
     return (
         <div>
             <Header/>
-            <div>
-                <h1>soy estudiante</h1>
+            <div className="education" id="education">
+                <h2 className="heading"> My <span>Journey</span></h2>
+                <div className="education-row">
+                    <div className="education-column">
+                        <h3 className="title">Education</h3>
+                        <div className="education-box">
+                            <div className="education-content">
+                                <div className="content">
+                                    <div className="year"><box-icon name='calendar' type='solid' ></box-icon>2022 - 2023</div>
+                                    <h3>SoyHenry</h3>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                </div>
+                            </div>
+                            <div className="education-content">
+                                <div className="content">
+                                    <div className="year"><box-icon name='calendar' type='solid' ></box-icon>2022 - 2023</div>
+                                    <h3>FreeCodeCamp</h3>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                </div>
+                            </div>
+                            <div className="education-content">
+                                <div className="content">
+                                    <div className="year"><box-icon name='calendar' type='solid'></box-icon>2022</div>
+                                    <h3>Centro de Formación Profesional 421 UOM La Plata</h3>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
