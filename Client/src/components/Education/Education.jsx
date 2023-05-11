fc88f13933da123cc499b83ea38e65f51ba0e72b
FranAchard <franachard8@gmail.com>
2023-05-10 21:17:12 -0300

    Educacion

--- Client/src/components/Education/Education.jsx
+++ Client/src/components/Education/Education.jsx
@@ -15,7 +15,7 @@ const Education =()=>{
                                 <div className="content">
                                     <div className="year"><box-icon name='calendar' type='solid' ></box-icon>2022 - 2023</div>
                                     <h3>SoyHenry</h3>
-                                    <p>velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                    <p></p>
                                 </div>
                             </div>
                             <div className="education-content">
@@ -31,7 +31,7 @@ const Education =()=>{
                                     <div className="year"><box-icon name='calendar' type='solid'></box-icon>2022</div>
                                     <h3>Curso de formacion profesional de Programador Web</h3>
                                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
-                                    <img className="titulo-img" src="/public/assets/curso-uom.png"></img>
+                                    <img className="titulo-img" src="./assets/curso-uom.png"></img>
                                 </div>
                             </div>
                         </div>
--- Client/src/components/Principal/Principal.jsx
+++ Client/src/components/Principal/Principal.jsx
@@ -15,7 +15,7 @@ const Principal=()=>{
                 </div>
             </div>
             <div className="home-sci">
-                <a href="https://github.com/FranAchard"><box-icon type='logo' name='github'></box-icon></a>
+                <a  href="https://github.com/FranAchard"><box-icon  type='logo' name='github'></box-icon></a>
                 <a href="https://www.linkedin.com/in/francisco-achard-9862641b3/"><box-icon name='linkedin' type='logo' ></box-icon></a>
                 <a href="https://www.instagram.com/_fran_achard/"><box-icon name='instagram-alt' type='logo' ></box-icon></a>
             </div>
