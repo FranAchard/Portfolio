14fd2f54d5ab69d5e3e5a8e00d10520d8ce895ec
FranAchard <franachard8@gmail.com>
2023-05-10 22:53:41 -0300

    educaion

--- Client/src/components/Education/Education.jsx
+++ Client/src/components/Education/Education.jsx
@@ -20,17 +20,21 @@ const Education =()=>{
                             </div>
                             <div className="education-content">
                                 <div className="content">
-                                    <div className="year"><box-icon name='calendar' type='solid' ></box-icon>2022 - 2023</div>
+                                    <div className="year"><box-icon name='calendar' type='solid' ></box-icon>2023</div>
                                     <h3>FreeCodeCamp</h3>
-                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                    <p>The course I took was a very enriching experience, with a duration of 300 hours. During this time, I had the opportunity to reinforce my knowledge in the field of web development and to learn new tools and techniques to improve my work as a programmer.
+
+                                        The course was focused on learning the fundamental sciences of web development, such as HTML and CSS. Through practice and the use of specialized tools, I learned to create and design web pages effectively, optimizing their appearance and functionality.</p>
                                     <img className="titulo-img" src="/public/assets/titulo-free.png" alt="Free-code-camp"></img>
                                 </div>
                             </div>
                             <div className="education-content">
                                 <div className="content">
                                     <div className="year"><box-icon name='calendar' type='solid'></box-icon>2022</div>
-                                    <h3>Curso de formacion profesional de Programador Web</h3>
-                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum perspiciatis culpa praesentium. Voluptate, nisi at amet, velit a omnis neque architecto quisquam reiciendis ratione eum consectetur quo officiis mollitia odit. </p>
+                                    <h3>Web Programmer Professional Training Course</h3>
+                                    <p>The course that I took over a period of 6 months was an enriching experience in my path as a programmer. I learned to use essential languages in web development, such as HTML, CSS, JavaScript and SQL, which allowed me to understand the structure, design and functionality of web pages.
+                                        Besides being my first approach to the world of programming, the course gave me the opportunity to consolidate my knowledge and skills in the creation of web pages. From the beginning of the course, I learned how to create a web page from scratch, adding styles, images, content and logic to make it work properly.
+                                        In summary, the course I took was an enriching experience that allowed me to acquire new knowledge in the field of web programming and improve my technical skills to develop web pages effectively.</p>
                                     <img className="titulo-img" src="./assets/curso-uom.png"></img>
                                 </div>
                             </div>
