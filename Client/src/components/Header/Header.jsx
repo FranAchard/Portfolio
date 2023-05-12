782b42c74b3bc7add8783c8bbd5e16fa873d793e
FranAchard <franachard8@gmail.com>
2023-05-12 01:44:53 -0300

    routes en el header

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import "boxicons"
 const Header = () => {
     return (
         <header className="header">
-            <a href="#" className="logo" >Francisco Achard</a>
+            <a href="https://portfolio-three-amber-54.vercel.app" className="logo" >Francisco Achard</a>
             <box-icon name='menu' id="menu-icon"></box-icon>
             <nav className="navBar">
-                <a href="http://localhost:3000" className="active">Home</a>
+                <a href="https://portfolio-three-amber-54.vercel.app" className="active">Home</a>
                 <a href="https://portfolio-three-amber-54.vercel.app/about" className="active">About</a>
-                <a href="http://localhost:3000/education" className="active">Education</a>
-                <a href="http://localhost:3000/skills" className="active">Skills</a>
-                <a href="http://localhost:3000/contact" className="active">Contact</a>
+                <a href="https://portfolio-three-amber-54.vercel.app/education" className="active">Education</a>
+                <a href="https://portfolio-three-amber-54.vercel.app/skills" className="active">Skills</a>
+                <a href="https://portfolio-three-amber-54.vercel.app/contact" className="active">Contact</a>
             </nav>
         </header>
     )
